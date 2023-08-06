6710a3653acdd5a0f4bd46d27a71c2bd21c0c91a
nyeroni <nir2y@naver.com>
2023-08-06 23:33:07 +0900

    commit

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,15 +18,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne //order <-> member 다대일 관계
+    @ManyToOne(fetch = FetchType.LAZY) //order <-> member 다대일 관계
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;//주문상태 [Order, Cancle]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
