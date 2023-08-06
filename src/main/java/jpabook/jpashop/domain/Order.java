17a9c2991aeea67b6a370c3d29b997107c595820
nyeroni <nir2y@naver.com>
2023-08-06 22:27:34 +0900

    commit

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,13 +19,19 @@ public class Order {
     private Long id;
 
     @ManyToOne //order <-> member 다대일 관계
+    @JoinColumn(name="member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate; //주문시간
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;//주문상태 [Order, Cancle]
 }
