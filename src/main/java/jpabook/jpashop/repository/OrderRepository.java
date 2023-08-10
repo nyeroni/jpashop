3775d99bc332778fe266f3a2114d589f80b34a7e
nyeroni <nir2y@naver.com>
2023-08-10 16:30:35 +0900

    commit

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -33,7 +33,7 @@ public class OrderRepository {
      */
     public List<Order> findAllByString(OrderSearch orderSearch) {
         //language = JPAQL
-        String jpql = "select o From Order o join o.member";
+        String jpql = "select o From Order o join o.member m";
         boolean isFirstCondition = true;
 
         //주문 상태 검색
